80b3f04aab5d499030d6671c0dfd4dbbfb712bf3
Tyler Ouellette <ouell117@uwindsor.ca>
2020-03-15 22:49:59 -0400

    added some styles to consider

--- pages/index.js
+++ pages/index.js
@@ -11,6 +11,29 @@ html {
 
 //https://thevirustracker.com/api
 
+/*  Extra Colour #/0f1419 I like with the https://dt-cdn.net/images/index-header-gradient-30474b72ac.svg
+display: inherit;
+    -ms-flex-direction: column;
+    flex-direction: column;
+    -ms-flex-pack: center;
+    justify-content: center;
+    -ms-flex-preferred-size: calc(100% - 1.25rem)!important;
+    flex-basis: calc(100% - 1.25rem)!important;
+    max-width: calc(100% - 1.25rem)!important;
+    position: relative;
+    padding: 1.5rem;
+    overflow: hidden;
+    transition: background-color .45s ease;
+    transition-property: background-color;
+    transition-duration: 0.45s;
+    transition-timing-function: ease;
+    transition-delay: 0s;
+    background: rgba(255,255,255,.03)!important;
+    border: 2px solid rgba(255,255,255,.25);
+    box-shadow: 0 0 30px 0 rgba(0,0,0,.4);
+    b
+*/
+
 export default function IndexPage() {
 	return (
 		<div>
